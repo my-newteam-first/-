f0253b13aed0f867e1c9b9db7c58acdabc13cd8f
Cook-R <35907848+Cook-R@users.noreply.github.com>
2018-05-05 12:30:25 +0800

    Signed-off-by: Cook-R <35907848+Cook-R@users.noreply.github.com>

---
+++ DAL/DLLLogin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class DllLogin
+    {
+        // 查询  DllLoginUser   entity userID,string type
+    }
+}
